c0e711ba28dd186bea946fedb4e4934f5ba4818e
Li Kui <90845831+likui628@users.noreply.github.com>
2022-10-10 21:24:32 +0800

    chore(deps): mybatis -> spring-data-jpa

--- src/main/java/com/example/realworld/RealworldApplication.java
+++ src/main/java/com/example/realworld/RealworldApplication.java
@@ -1,12 +1,19 @@
 package com.example.realworld;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RealworldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RealworldApplication.class, args);
+	}
+
+	@Bean
+	Faker faker(){
+		return new Faker();
 	}
 }
