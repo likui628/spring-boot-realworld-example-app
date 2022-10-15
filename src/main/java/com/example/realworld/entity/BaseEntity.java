d94303f84d3f1bc42a04e59558f1247e5bb94325
Li Kui <90845831+likui628@users.noreply.github.com>
2022-10-15 19:35:53 +0800

    feat: jpa auditing

--- src/main/java/com/example/realworld/entity/BaseEntity.java
+++ src/main/java/com/example/realworld/entity/BaseEntity.java
@@ -2,14 +2,28 @@ package com.example.realworld.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    protected LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    protected LocalDateTime updatedAt;
 }
