59751902d7911a5437e7c2b3ddae23301e9e8de3
Li Kui <90845831+likui628@users.noreply.github.com>
2022-10-16 09:01:36 +0800

    chore: faker user & application properties

--- src/main/java/com/example/realworld/RealworldApplication.java
+++ src/main/java/com/example/realworld/RealworldApplication.java
@@ -1,13 +1,20 @@
 package com.example.realworld;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RealworldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RealworldApplication.class, args);
+    }
+
+    @Bean
+    public Faker getFaker(){
+        return new Faker();
     }
 
 }
