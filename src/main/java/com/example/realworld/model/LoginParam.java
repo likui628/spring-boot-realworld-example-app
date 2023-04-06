e5eca6020a6ba317384bf5fa819973c2151f31c6
Li Kui <90845831+likui628@users.noreply.github.com>
2023-04-06 20:59:54 +0800

    chore: switch to mybatis

--- src/main/java/com/example/realworld/model/LoginParam.java
+++ src/main/java/com/example/realworld/model/LoginParam.java
@@ -1,13 +1,13 @@
 package com.example.realworld.model;
 
 import com.fasterxml.jackson.annotation.JsonRootName;
-import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
--- src/main/java/com/example/realworld/model/RegistrationParam.java
+++ src/main/java/com/example/realworld/model/RegistrationParam.java
@@ -1,15 +1,11 @@
 package com.example.realworld.model;
 
 import com.fasterxml.jackson.annotation.JsonRootName;
-import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Getter
 @AllArgsConstructor
