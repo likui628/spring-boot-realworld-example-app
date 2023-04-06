496050f8173da7e4900e66574b9db125d7676cf3
Li Kui <90845831+likui628@users.noreply.github.com>
2023-04-06 21:35:25 +0800

    chore: mysql

--- src/main/java/com/example/realworld/RealworldApplication.java
+++ src/main/java/com/example/realworld/RealworldApplication.java
@@ -1,9 +1,7 @@
 package com.example.realworld;
 
-import com.github.javafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RealworldApplication {
@@ -11,10 +9,4 @@ public class RealworldApplication {
     public static void main(String[] args) {
         SpringApplication.run(RealworldApplication.class, args);
     }
-
-    @Bean
-    public Faker getFaker() {
-        return new Faker();
-    }
-
 }
